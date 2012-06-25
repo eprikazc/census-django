8fc2f24cbb31cbe7d24707ba1515676c6c00b8b8
Eugene Prikazchikov <eprikazc@gmail.com>
2012-06-25 23:16:51 +0400

    Added pie chart for Vacancy Status

--- census_data/static/census.js
+++ census_data/static/census.js
@@ -15,6 +15,7 @@ $(function(){
     $("button.get_data").click(function(event){
         event.preventDefault();
         $("#table-area").html("");
+        $("#plot-area").html("");
         var geo_region_selected = $("option:selected", $(this).siblings("select"));
         $.ajax({
             url: "/stat/" + geo_region_selected.attr("value") + "/",
@@ -26,6 +27,49 @@ $(function(){
                 );
                 $("#table-area").html(table_data);
                 $("#tabs").tabs();
+                var chart;
+                var chart_data = [];
+                for (var i=1; i<data["VACANCY STATUS [8]"].length; i++)
+                {
+                    chart_data.push([data["VACANCY STATUS [8]"][i][1], parseFloat(data["VACANCY STATUS [8]"][i][2])]);
+                }
+                chart = new Highcharts.Chart({
+                    chart: {
+                        renderTo: 'plot-area',
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false
+                    },
+                    title: {
+                        text: 'Vacancy status pie chart'
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            return '<b>'+ this.point.name +'</b>: '+ this.y;
+                        }
+                    },
+                    plotOptions: {
+                        pie: {
+                            animation: false,
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                color: '#000000',
+                                connectorColor: '#000000',
+                                formatter: function() {
+                                    return '<b>'+ this.point.name +'</b>: '+ this.y;
+                                }
+                            }
+                        }
+                    },
+                    series: [{
+                        type: 'pie',
+                        name: 'Vacancy status',
+                        data: chart_data
+                    }]
+                });
+
             },
             error: function(data){
                 $("#table-area").html("<p style='color:red'>Error loading data</p>");
