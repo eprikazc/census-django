0ce570598f040eec26dc51146e9fd5eb6a868e66
Eugene Prikazchikov <eprikazc@gmail.com>
2012-06-28 10:20:36 +0400

    Added charts to reports

--- census_data/templatetags/extras.py
+++ census_data/templatetags/extras.py
@@ -7,6 +7,7 @@ def clean_stat_name(value):
 
 @register.filter(name='add_commas')
 def add_commas(value):
+    value = str(value)
     res = ""
     for i in range(len(value), 0, -1):
         res = "%s%s" %(value[i-1], res)
@@ -16,4 +17,7 @@ def add_commas(value):
 
 @register.assignment_tag(takes_context=True)
 def get_context_value(context, key_name):
-    return context[key_name]
+    data = []
+    for elem in context[key_name]:
+        data.append([elem[0], elem[1], int(elem[2])])
+    return data
